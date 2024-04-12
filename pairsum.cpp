53483a6b533848a5ee574a343b5178ca5304baa6
chikkisharma <124499138+chikkisharma@users.noreply.github.com>
2024-04-12 20:52:02 +0530

    Update pairsum.cpp

--- pairsum.cpp
+++ pairsum.cpp
@@ -1,8 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int arr[5]={1,2,3,4,5};
-    int target=3;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
     for(int i=0;i<5;i++){
         for(int j=i+1;j<5;j++){
             if(arr[i]+arr[j]==target){
