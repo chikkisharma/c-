8769b36c7dd6ff459537df2bb2e80705a96457ef
chikkisharma <124499138+chikkisharma@users.noreply.github.com>
2024-04-16 18:00:51 +0530

    Add files via upload

---
+++ sortzeroone.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sortZeroOne(int arr[],int n){
+    int i=0;
+    int j=n-1;
+    while(i<=j){
+        if(arr[i]==0){
+            i++;
+        }
+        else if(arr[i]==1){
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+    }
+}
+int main(){
+    int arr[9]={0,1,0,1,1,0,0,0,0};
+    int n =9;
+
+    sortZeroOne(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
