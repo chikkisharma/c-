ad5cf57c89d24d281152d5cfd88491d9f3f32e4a
chikkisharma <124499138+chikkisharma@users.noreply.github.com>
2024-04-18 00:39:16 +0530

    Update SortAnArrayof0sand1s.cpp

--- SortAnArrayof0sand1s.cpp
+++ SortAnArrayof0sand1s.cpp
@@ -7,7 +7,10 @@ void sortZeroOne(int arr[],int n){
         if(arr[i]==0){
             i++;
         }
-        else if(arr[i]==1){
+        else if(arr[j]==1){
+            j--;
+        }
+        else if(arr[i]==1 && arr[j]==0){
             swap(arr[i],arr[j]);
             i++;
             j--;
